39c2b471d9024d041a90b764c839251a6df16bb8
Anselm Schüler <mail@anselmschueler.com>
2022-08-10 13:50:01 +0200

    Fubar

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let debug = false;
+
+    println!("Welcome to the number guessing game");
+
+    let secret_number: u16 = rand::thread_rng().gen_range(1..=100);
+    if debug {
+        eprintln!("@debug secret_number = {secret_number}")
+    }
+
+    loop {
+        print!("Please enter a guess: ");
+        io::stdout().flush().expect("Failure to write to output");
+
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Cannot read input");
+
+        let guess: u16 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {guess}.");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+        }
+    }
+}
